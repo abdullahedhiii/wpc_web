cdd9be4bc2544301fbf2443a19e9eb33bf6f2a67
Muhammad Hamza Edhi <abdulanis190@gmail.com>
2025-03-07 16:58:07 +0500

    CORS issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:3300', 
+        target: process.env.VITE_API_URL, 
         rewrite: (path) => path.replace(/^\/api/, ''), 
         secure: false, 
         changeOrigin: true, 
